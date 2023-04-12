7633da21ab40f8feddd4bbce10cbd0c451eede80
Aloisius <kalosius43@gmail.com>
2023-04-12 10:29:53 +0300

    changed folders of afro_dj and afro_django

---
+++ afro_dtl/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    p_title ="Afro-Django"
+    username = 'Aloisius'
+    gender = 'Male'
+    return render(
+        request, 
+        'index.html',
+        {'p_title': p_title, 'username':username, 'gender':gender}
+    )
+
+def register(request):
+    return render(request, 'register.html')
