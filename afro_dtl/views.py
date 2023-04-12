914855f5afdee8bb242161d1309e2f2a3ba4b627
Aloisius <kalosius43@gmail.com>
2023-04-12 09:30:47 +0300

    added login, register

---
+++ afro_dtl/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    p_title ="Afro-Django"
+    username = 'Aloisius'
+    gender = 'Male'
+    return render(
+        request, 
+        'index.html',
+        {'p_title': p_title, 'username':username, 'gender':gender}
+    )
